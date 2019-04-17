traefik/traefik

Forward all header values from forward auth response

middlewares/auth/forward.go
@@ -96,7 +96,11 @@ func Forward(config *types.Forward, w http.ResponseWriter, r *http.Request, next
 	}
 
 	for _, headerName := range config.AuthResponseHeaders {
-		r.Header.Set(headerName, forwardResponse.Header.Get(headerName))
+		headerKey := http.CanonicalHeaderKey(headerName)
+		r.Header.Del(headerKey)
+		if len(forwardResponse.Header[headerKey]) > 0 {
+			r.Header[headerKey] = append([]string(nil), forwardResponse.Header[headerKey]...)
+		}
 	}
 
 	r.RequestURI = r.URL.RequestURI()

alfonsodg/traefik-api-srv

pkg/middlewares/auth/forward.go
@@ -147,7 +147,11 @@ func (fa *forwardAuth) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	}
 
 	for _, headerName := range fa.authResponseHeaders {
-		req.Header.Set(headerName, forwardResponse.Header.Get(headerName))
+		headerKey := http.CanonicalHeaderKey(headerName)
+		req.Header.Del(headerKey)
+		if len(forwardResponse.Header[headerKey]) > 0 {
+			req.Header[headerKey] = append([]string(nil), forwardResponse.Header[headerKey]...)
+		}
 	}
 
 	req.RequestURI = req.URL.RequestURI()